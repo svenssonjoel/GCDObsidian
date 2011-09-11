475738e5a39333280b44c81ce70558b76bf2a341
Joel Svensson <svensson.bj@gmail.com>
2011-09-11 18:25:40 +0200

    moving the simplified example towards the real deal

--- Simplified_situation.hs
+++ Simplified_situation.hs
@@ -1,15 +1,21 @@
 
 {-# LANGUAGE TypeFamilies, 
-             FlexibleInstances #-} 
+             FlexibleInstances,  
+             FlexibleContexts,
+             UndecidableInstances,
+             GADTs #-} 
 
-module Test1 where 
+module Simplified_situation where 
 
 import Data.List
 
 
+data Expr a where
+  Literal :: a -> Expr a 
+  Tuple :: (Expr a) -> (Expr b) -> Expr (a,b) 
 
 
-data Array a = Array (Int -> a) Int 
+data Array a = Array (Int -> (Expr a)) Int 
 len (Array _ n) = n
 (!) (Array ixf _) ix = ixf ix
 
@@ -22,23 +28,24 @@ program2 :: (Array Int,Array Int) -> Array Int
 program2 (arr1,arr2) = program arr1
 
 zipper :: (Array Int, Array Int) -> Array (Int,Int) 
-zipper (a1,a2) = Array (\ix -> (a1 ! ix, a2 ! ix)) (len a1)
+zipper (a1,a2) = Array (\ix -> Tuple (a1 ! ix) (a2 ! ix)) (len a1)
 
 
 type family ExecIO a 
-type instance ExecIO Int         = Int 
-type instance ExecIO (Array a)   = [ExecIO a]
-type instance ExecIO (a,b) = (ExecIO a, ExecIO b) 
+type instance ExecIO (Expr Int)   = Int 
+type instance ExecIO (Array a)    = [ExecIO (Expr a)]
+type instance ExecIO (a,b)        = (ExecIO a, ExecIO b) 
+type instance ExecIO (Expr (a,b)) = (ExecIO (Expr a),ExecIO (Expr b))
 
 class Exec a where 
   toExec :: ExecIO a -> a 
   fromExec :: a -> ExecIO a
   
-instance Exec Int where   
-  toExec i = i 
-  fromExec i = i 
+instance Exec (Expr Int) where   
+  toExec i = Literal i 
+  fromExec (Literal i) = i 
   
-instance Exec a => Exec (Array a) where 
+instance Exec (Expr a) => Exec (Array a) where 
   toExec ls = Array (\ix -> toExec (ls !! ix)) (length ls) 
   fromExec arr = [fromExec (arr ! i) | i <- [0.. len arr-1]]
 
@@ -46,6 +53,10 @@ instance (Exec a, Exec b) => Exec (a,b) where
   toExec (a,b) = (toExec a, toExec b) 
   fromExec (a,b) = (fromExec a, fromExec b) 
 
+instance (Exec (Expr a), Exec (Expr b)) => Exec (Expr (a,b)) where
+  toExec (i1,i2) = Tuple (toExec i1)  (toExec i2)
+  fromExec (Tuple e1 e2) = (fromExec e1,fromExec e2)
+         
 
 
 execute :: (Exec a, Exec b)  => (a -> b) -> (ExecIO a) -> (ExecIO b) 
