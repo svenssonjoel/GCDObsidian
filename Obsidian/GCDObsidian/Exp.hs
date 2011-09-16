2bfa72c2c74a0e2312deec7341550802ad21aeed
Joel Svensson <svensson.bj@gmail.com>
2011-09-16 15:23:31 +0200

    added small notes

--- Obsidian/GCDObsidian/Exp.hs
+++ Obsidian/GCDObsidian/Exp.hs
@@ -24,7 +24,7 @@ import Obsidian.GCDObsidian.Elem
 data Type = Bool | Int | Word8 | Word16 | Word32 | Word64 
           | Float | Double                     
                     
-          -- Used by CUDA And OpenCL generators          
+          -- Used by CUDA, C And OpenCL generators          
           | Pointer Type   -- C thing 
           | Global Type    -- OpenCL thing
           | Local Type     -- OpenCL thing
