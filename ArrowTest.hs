51c07e23e482e855ed67d73cf64e4d68da6dfdcb
Joel Svensson <svensson.bj@gmail.com>
2011-10-04 13:28:06 +0200

    expanded a bit on AddOnArrow

---
+++ ArrowTest.hs
@@ -0,0 +1,44 @@
+module ArrowTest where 
+
+
+import Obsidian.GCDObsidian
+import Obsidian.GCDObsidian.AddOn.ArrowObsidian 
+import Obsidian.GCDObsidian.CodeGen.CUDA as CUDA 
+
+import Control.Category
+import Prelude hiding ((.),id)
+
+
+-- Most simple test
+test1 :: Array (Exp Int) -> Kernel (Array (Exp Int)) 
+test1 = runArrow id
+
+getTest1 = putStrLn$ CUDA.genKernel "test1" test1 (namedArray "apa" 32)
+
+
+-- Test that uses things from Obsidian standard library
+-- (these are reusable in the ArrowAddOn
+test2 :: Array (Exp Int) -> Kernel (Array (Exp Int)) 
+test2 = runArrow$ Pure rev . Pure (fmap (+1))  
+
+getTest2 = putStrLn$ CUDA.genKernel "test2" test2 (namedArray "apa" 32)
+
+
+-- Test that uses both AddOn and base Obsidian
+test3 :: Array (Exp Int) -> Kernel (Array (Exp Int))
+test3  = pure (fmap (+1)) ->- (runArrow (Pure (fmap (+1))))   
+  
+getTest3 = putStrLn$ CUDA.genKernel "test3" test3 (namedArray "apa" 32)
+
+
+-- Same as above but with a sync
+test4 :: Array (Exp Int) -> Kernel (Array (Exp Int))
+test4  = pure (fmap (+1)) ->- sync ->- (runArrow (Pure (fmap (+1))))   
+  
+getTest4 = putStrLn$ CUDA.genKernel "test4" test4 (namedArray "apa" 32)
+
+
+{- These tests show the need for a few things. 
+   #1 overloaded pure
+   #2 overloaded sync ? 
+   #3 use . for composition in base Obsidian also for uniform look 
--- Obsidian/GCDObsidian/AddOn/ArrowObsidian.hs
+++ Obsidian/GCDObsidian/AddOn/ArrowObsidian.hs
@@ -10,17 +10,28 @@ import Obsidian.GCDObsidian.Program
 import Obsidian.GCDObsidian.Array
 import Obsidian.GCDObsidian.Sync
 
+import Control.Category
+import Prelude hiding ((.),id)
 
 {- an attempt at implementing old ArrowObsidian as a 
    library on top of new "MonadObsidian" (GCDObsidian) -} 
 
 data a :-> b where 
   Pure :: (a -> b) -> (a :-> b) 
-  Sync :: Syncable Array b => (a -> Array b) -> (Array b -> c) -> 
-          (a :-> c) 
+  Sync :: Syncable Array b 
+          => (a -> Array b) 
+          -> (Array b -> c) 
+          -> (a :-> c) 
           
+                    
+instance Category (:->) where
+  id = Pure id 
+  
+  (.) (Pure f) (Pure g) = Pure (f . g) 
+  (.) (Pure f) (Sync h g) = Sync h (f . g)
+  (.) (Sync h g) (Pure f) = Sync (h . f) g 
           
-          
+                            
 runArrow :: (a :-> b) -> a -> Kernel b 
 runArrow (Pure f) a   = return$ f a
 runArrow (Sync f g) a = 
