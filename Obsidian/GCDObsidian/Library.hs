a19843fcd68052ff40986cd675134d719139f5e8
Joel Svensson <svensson.bj@gmail.com>
2011-11-29 14:17:16 +0100

    cleaning in library

--- Obsidian/GCDObsidian/Library.hs
+++ Obsidian/GCDObsidian/Library.hs
@@ -234,13 +234,15 @@ everyOther f  = \(ix,(a,b)) -> f (ix * 2,a) *>* f (ix * 2 + 1,b)
     
 zipP :: Pushy arr  => arr a -> arr a -> Array Push a  
 zipP arr1 arr2 =
-  Array (Push (\func -> (f (\(i,a) -> func (2*i,a)))
-                   *>*
-                   (g (\(i,a) -> func (2*i + 1,a)))))
+  mkPushArray (\func -> p1 !* (\(i,a) -> func (2*i,a))
+                        *>*
+                        p2 !* (\(i,a) -> func (2*i + 1,a)))
          (n1+n2)
   where 
-    (Array (Push f) n1) = push arr1
-    (Array (Push g) n2) = push arr2
+    p1 = push arr1
+    p2 = push arr2
+    n1 = len p1
+    n2 = len p2 
     
     
     
@@ -250,22 +252,22 @@ zipP arr1 arr2 =
 -- length is being pushed to. 
 -- Should definitely not be exposed to the outside. 
 combine :: Array Push a -> Array Push a -> Array Push a    
-combine (Array a1 n1) (Array a2 n2) = 
-  Array (Push (\k -> pushFun a1 k *>* pushFun a2 k)) (n1 + n2) 
+combine p1 p2 = 
+  mkPushArray (\k -> p1 !* k *>* p2 !* k) (len p1 + len p2) 
     
   
   
 ----------------------------------------------------------------------------  
 -- The oposite to ivDiv    
 ivMerge :: Pushy arr => Int -> Int -> arr a -> arr a -> Array Push a
-ivMerge i j arr1 arr2 = Array (Push (\k -> app a1 k *>* app a2 k)) (len a1 + len a2) 
+ivMerge i j arr1 arr2 = mkPushArray (\k -> a1 !* k *>* a2 !* k) (len a1 + len a2) 
   where
     left ix = ix + (ix .&. complement (oneBits (i+j)))
     right ix = (left ix) `xor` (fromIntegral mask)
     mask = (oneBits (j+1) :: Word32) `shiftL` i
     a1 = ixMap left (push arr1)
     a2 = ixMap right (push arr2)
-    app (Array f _) a = pushFun f a 
+   
   
 ----------------------------------------------------------------------------
 -- iv  a sorter building block
@@ -308,125 +310,88 @@ lowBit i ix = (ix .&. bit i) ==* 0
 flipLSBsTo :: Int -> UWordE -> UWordE
 flipLSBsTo i = (`xor` (oneBits (i+1)))
 
-
-
-  
-
+----------------------------------------------------------------------------
+-- 
 ilv1 :: Choice a => Int -> (b -> b-> a) -> (b -> b -> a) -> Array Pull b -> Array Pull a
-ilv1 i f g arr = Array (Pull ixf) (len arr)
+ilv1 i f g arr = mkPullArray ixf (len arr)
   where
     ixf ix = let l = arr ! ix
                  r = arr ! newix
                  newix = flipBit i ix
-             in (ifThenElse (lowBit i ix) (f l r) (g l r))
+             in ifThenElse (lowBit i ix) (f l r) (g l r)
 
 vee1 :: Choice a => Int -> (b -> b-> a) -> (b -> b -> a) -> Array Pull b -> Array Pull a
-vee1 i f g arr = Array (Pull ixf) (len arr)
+vee1 i f g arr = mkPullArray ixf (len arr)
   where
     ixf ix = let l = arr ! ix
                  r = arr ! newix
                  newix = flipLSBsTo i ix
-             in (ifThenElse (lowBit i ix) (f l r) (g l r))
+             in ifThenElse (lowBit i ix) (f l r) (g l r)
 
 ilvVee1 :: Choice a => Int -> Int -> (b -> b-> a) -> (b -> b -> a) -> Array Pull b -> Array Pull a
-ilvVee1 i j f g arr = Array (Pull ixf) (len arr)
+ilvVee1 i j f g arr = mkPullArray ixf (len arr)
   where
     ixf ix = let l = arr ! ix
                  r = arr ! newix
                  newix = flipBitsFrom i j ix
-             in (ifThenElse (lowBit (i+j) ix) (f l r) (g l r))
-
-
-    
+             in ifThenElse (lowBit (i+j) ix) (f l r) (g l r)
 
 
 
+----------------------------------------------------------------------------
+--
 ilv2 :: Choice b => Int -> (a -> a -> b) -> (a -> a -> b) -> 
                     Array Pull a -> Array Push b
-ilv2 i f g (Array (Pull ixf) n) 
-   = Array (Push (\k -> app a5 k *>* app a6 k)) n
+ilv2 i f g arr 
+   = mkPushArray (\k -> a5 !* k *>* a6 !* k) n
   where
+    n  = len arr
     n2 = n `div` 2
-    a1 = Array (Pull (ixf . left)) (n-n2)
-    a2 = Array (Pull (ixf . right)) n2
+    a1 = resize (ixMapPull left arr) (n-n2) -- Array (Pull (ixf . left)) (n-n2)
+    a2 = resize (ixMapPull right arr) (n2)  -- Array (Pull (ixf . right)) n2
     a3 = zipWith f a1 a2
     a4 = zipWith g a1 a2
     a5 = ixMap left (push a3)
     a6 = ixMap right (push a4)
     left = insertZero i
     right = flipBit i  . left
-    app (Array (Push f) _) a = f a
+ 
 
-
-
-
-
-
+----------------------------------------------------------------------------
+-- 
 vee2 :: Choice b => Int -> (a -> a -> b) -> (a -> a -> b) -> 
                     Array Pull a -> Array Push b
-vee2 i f g (Array (Pull ixf) n) 
-   = Array (Push (\k -> app a5 k *>* app a6 k)) n
+vee2 i f g arr  
+   = mkPushArray (\k -> a5 !* k *>* a6 !* k) n
   where
+    n  = len arr
     n2 = n `div` 2
-    a1 = Array (Pull (ixf . left)) (n-n2)
-    a2 = Array (Pull (ixf . right)) n2
+    a1 = resize (ixMapPull left arr) (n-n2) -- Array (Pull (ixf . left)) (n-n2)
+    a2 = resize (ixMapPull right arr) n2    -- Array (Pull (ixf . right)) n2
     a3 = zipWith f a1 a2
     a4 = zipWith g a1 a2
     a5 = ixMap left (push a3)
     a6 = ixMap right (push a4)
     left = insertZero i
     right = flipLSBsTo i  . left
-    app (Array (Push f) _) a = f a
+
 
    
 
 ilvVee2 :: Choice b => Int -> Int -> (a -> a -> b) -> (a -> a -> b) -> 
             Array Pull a -> Array Push b
-ilvVee2 i j f g (Array (Pull ixf) n) 
-   = Array (Push (\k -> app a5 k *>* app a6 k)) n
+ilvVee2 i j f g arr 
+   = mkPushArray (\k -> a5 !* k *>* a6 !* k) n
   where
+    n  = len arr
     n2 = n `div` 2
-    a1 = Array (Pull (ixf . left)) (n-n2)
-    a2 = Array (Pull (ixf . right)) n2
+    a1 = resize (ixMapPull left arr) (n-n2) -- Array (Pull (ixf . left)) (n-n2)
+    a2 = resize (ixMapPull right arr) n2    -- Array (Pull (ixf . right)) n2
     a3 = zipWith f a1 a2
     a4 = zipWith g a1 a2
     a5 = ixMap left (push a3)
     a6 = ixMap right (push a4)
     left = insertZero (i+j)
     right = flipBitsFrom i j . left
-    app (Array (Push f) _) a = f a
+   
 
-----------------------------------------------------------------------------
--- Old
-{- 
-insertZero :: Int -> Exp Word32 -> Exp Word32
-insertZero i a = a + (a .&. fromIntegral (complement (oneBits i :: Word32)))
-
-flipBits :: Bits a => Int -> Int -> a -> a
-flipBits i j a = a `xor` (fromIntegral mask)
-  where
-    mask = (oneBits j :: Word32) `shiftL` i
-
-
-
-oneBits i = bit i - 1
-    
-    
-    
-    
-
--- The oposite to ivDiv    
-
-    
-{-    
--- iv  a sorter building block
-iv i j f g arr = part
-  where
-    (arr1,arr2) = ivDiv i j arr
-    part = ivMerge i j arr1' arr2'
-    arr1' = push $ zipWith f arr1 arr2
-    arr2' = push $ zipWith g arr1 arr2
-
--} 
-
--}
