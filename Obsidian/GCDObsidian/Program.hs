208c9623cc53164ad1dc9a8c77d6f5d27822a2cc
Josef Svenningsson <josef.svenningsson@gmail.com>
2012-10-17 11:30:29 +0200

    Enable do-notation when writing programs in the Program data type.
    I've opted for a minimally invasive way of implementing this. The Program
    data type is completely untouched and so there is very little that needs to
    change in other modules.
    
    There is a new P monad which enables writing things in do-notation. When
    running P using the runP function a Program tree will be produced. There are
    functions in the P monad which correspond to the constructors in the Program
    data type, such as 'forAll', 'allocate', 'assign' and so forth.
    
    The implementation of the P monad is rather subtle because it needs to
    generate fresh names on the go. Normally, when providing a monadic interface
    one can leave that to the code generator by including a bind constructor,
    corresponding to the (>>=) operator in the monad class. Since this constructor
    is higher order it doesn't have to worry about names. However, since I opted
    to go for a minimally invasive change I didn't introduce a bind constructor.
    Names are only needed in one place, and that is for the atomic operations. I
    figured that it would be too big of a change to rewrite big parts of Obsidian
    just to allow for atomic operations. Hence I decided to do the name generation
    in the P monad. It uses a NameSupply monad which provides fresh names. That
    monad in turn uses value supplies. At first I used a state monad to pass
    around a state but that turned out not to work for functions like 'forAll'
    where the monad needs to be run inside of a function. Using the trick with
    passing a splittable name supply solved the problem.
    
    Examples of using the do-notation:
    runP (forAll 32 (\i -> do {p <- atomicOp "a" i AtomicInc; assign "b" 0 p}))
    
    runP (do { assign "a" 32 (100 :: Data Word32); assign "b" 10 (40 :: Data Word32); a <- atomicOp "c" 0 AtomicInc; assign "d" 0 a})

--- Obsidian/GCDObsidian/Program.hs
+++ Obsidian/GCDObsidian/Program.hs
@@ -17,10 +17,14 @@ import Obsidian.GCDObsidian.Exp
 import Obsidian.GCDObsidian.Types
 import Obsidian.GCDObsidian.Globs
 
+-- Package value-supply
+import Data.Supply
+import System.IO.Unsafe
+
 ----------------------------------------------------------------------------
 -- 
 -- TODO: Data a and Exp a is the same. (look at this!) 
-data Program extra 
+data Program extra
   = Skip
     
   | forall a. Scalar a => Assign Name (Data Word32) (Data a) 
@@ -54,6 +58,7 @@ infixr 5 *>*
          -> Program extra    
 (*>*) = ProgramSeq 
     
+
 programThreads :: Program extra -> Word32
 programThreads Skip = 0
 programThreads (Synchronize _) = 0 
@@ -75,8 +80,8 @@ printProgram (Allocate name n t e) = name ++ " = malloc(" ++ show n ++ ")\n" ++
                                      "[*** " ++ show e ++ " ***]\n" 
 printProgram (ProgramSeq p1 p2) = printProgram p1 ++ printProgram p2
 -- Needs fresh name generations to be correct
-printProgram (AtomicOp name n i e) = name ++ " = " ++ printAtomic e ++
-                                     "(" ++ n ++ "[" ++ show i ++ "])"
+printProgram (AtomicOp d n i e) = d ++ " = " ++ printAtomic e ++
+                                     "(" ++ n ++ "[" ++ show i ++ "])\n"
 
 instance Show extra => Show (Program extra) where 
   show = printProgram 
@@ -96,3 +101,66 @@ data Atomic a where
   AtomicInc :: Atomic (Data Int)
 
 printAtomic AtomicInc = "atomicInc"
+
+
+----------------------------------------------------------------------
+-- A monadic interface to Program
+
+-- I've set extra = (). I hope that's ok, but it's not a big thing. It's eays
+-- to change.
+
+-- The extra Int argument is for generating fresh variable names
+
+data P a = P { unP :: (a -> NameSupply (Program ())) -> NameSupply (Program ())}
+
+instance Monad P where
+  return a  = P (\k -> k a)
+  P f >>= m = P (\k -> f (\a -> unP (m a) k))
+
+runP :: P a -> Program ()
+runP (P m) = unNS (m (\_ -> return Skip)) (unsafePerformIO (newEnumSupply))
+
+(*>>) :: Program () -> NameSupply (Program ()) -> NameSupply (Program ())
+p *>> m = do p' <- m
+             return (p *>* p')
+
+skip :: P ()
+skip = P (\k -> Skip *>> k ())
+
+assign :: Scalar a => Name -> Data Word32 -> Data a -> P ()
+assign name ix e = P (\k -> Assign name ix e *>> k ())
+
+forAll :: Word32 -> (Data Word32 -> P ()) -> P ()
+forAll l body = P (\k -> do b <- runFunc (\i -> unP (body i) (\_ -> return Skip))
+                            ForAll b l *>> k ())
+
+forAllGlobal :: Data Word32 -> (Data Word32 -> P ()) -> P ()
+forAllGlobal l body = P (\k -> do b <- runFunc (\i -> unP (body i) (\_ -> return Skip))
+                                  ForAllGlobal b l *>> k ())
+
+allocate :: Name -> Word32 -> Type -> P ()
+allocate name ix ty = P (\k -> Allocate name ix ty () *>> k ())
+
+synchronize :: P ()
+synchronize = P (\k -> Synchronize True *>> k ())
+-- TODO: what should the boolean parameter be?
+
+atomicOp :: Scalar a => Name -> Data Word32 -> Atomic (Data a) -> P (Data a)
+atomicOp name ix atomic = P (\k -> do dest <- newName "a"
+                                      AtomicOp dest name ix atomic *>> k (Index (dest,[])))
+
+-- A monad to generate fresh names for the variables assigned in the atomic
+-- operations
+data NameSupply a = NS { unNS :: Supply Int -> a }
+
+instance Monad NameSupply where
+  return a = NS $ \_ -> a
+  NS f >>= m = NS $ \s ->
+    let (s1,s2) = split2 s in
+    unNS (m (f s1)) s2
+
+newName :: String -> NameSupply String
+newName v = NS $ \s -> v ++ show (supplyValue s)
+
+runFunc :: (a -> NameSupply b) -> NameSupply (a -> b)
+runFunc f = NS $ \s -> \a -> unNS (f a) s
