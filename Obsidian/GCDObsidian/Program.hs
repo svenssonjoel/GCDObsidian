03e2e5e0afd3f2758637995cdc21135fe31461fe
Joel Svensson <svenssonjoel@yahoo.se>
2012-10-25 20:34:39 +0200

    Experimentation with Monads

--- Obsidian/GCDObsidian/Program.hs
+++ Obsidian/GCDObsidian/Program.hs
@@ -5,8 +5,8 @@ module Obsidian.GCDObsidian.Program
        ,printProgram
        ,programThreads
        ,(*>*)
-       ,targetArray
-       ,targetPair
+--       ,targetArray
+--       ,targetPair
        ,Atomic(..)
        ,printAtomic
 
@@ -33,110 +33,85 @@ import System.IO.Unsafe
 
 ----------------------------------------------------------------------------
 -- 
--- TODO: Data a and Exp a is the same. (look at this!) 
-data Program extra res where 
-  Skip :: Program extra () 
+-- TODO: Try to make this happen without the extra parameter.  
+data Program res where 
+  Skip :: Program () 
     
-  Assign :: forall extra a . Scalar a
+  Assign :: forall a . Scalar a
             => Name
             -> (Exp Word32)
             -> (Exp a)
-            -> Program extra ()
+            -> Program ()
            
             
-  AtomicOp :: forall extra a . Scalar a
+  AtomicOp :: forall a . Scalar a
               => Name 
               -> Exp Word32
               -> Atomic (Exp a)
-              -> Program extra (Exp a)
+              -> Program (Exp a)
 
   -- Shouldnt a ForAll have a different result type.
   -- Someting that represents an "array" 
   ForAll :: Word32
-            -> (Exp Word32 -> Program extra ())
-            -> Program extra ()
+            -> (Exp Word32 -> Program ())
+            -> Program ()
 
   --        -> Program extra [a]           
 
-  Allocate :: Word32 -> Type -> extra -> Program extra Name -- Correct type?
-  Sync     :: Program extra ()
+  Allocate :: Word32 -> Type -> Program Name -- Correct type?
+  Sync     :: Program ()
 
-  Return :: a -> Program extra a
-  Bind   :: (a -> Program extra b) -> Program extra a -> Program extra b
+  Return :: a -> Program a
+  Bind   :: (a -> Program b) -> Program a -> Program b
 
----------------------------------------------------------------------------
--- Tests
----------------------------------------------------------------------------
-
-testPrg1 :: Program () () 
-testPrg1 =
-  do
-    somewhere <- Allocate 100 Int () 
-    ForAll 100
-           (\i -> Assign somewhere i (variable "hej" :: Exp Int))
-
-testPrg2 :: Program () ()
-testPrg2 =
-  do
-    somewhere <- Allocate 175 Int () 
-    ForAll 175
-           (\i -> Assign somewhere i (variable "a" :: Exp Int))
-
-testPrg3 :: Program () ()
-testPrg3 = testPrg1 >> Sync >> testPrg2 
-
----------------------------------------------------------------------------
--- Small push/pull array test
----------------------------------------------------------------------------
-data PushArray a =
-  PA (forall b. ((Exp Word32, a) -> Program () ()) -> Program () ())
-data PullArray a = PU Word32 (Exp Word32 -> a) 
-
-myPullArray :: PullArray (Exp Int) 
-myPullArray = PU 100 (\ix -> (index "input0" ix) )
-
-push1 :: PullArray (Exp Int) -> PushArray (Exp Int)
-push1 (PU n ixf) =
-  PA (\k -> do
-         ForAll n
-           (\i -> k (i,ixf i)))
-
-writeP :: PushArray (Exp Int) -> Program () ()
-writeP (PA p) =
-  do
-    name <-  Allocate 100 Int ()
-    p (target name) 
-
-target :: Name -> (Exp Word32, Exp Int) -> Program () ()
-target name (ix,x) = Assign name ix x 
-
-testPrg4 = writeP (push1 myPullArray)
 ---------------------------------------------------------------------------
 -- Monad
 ---------------------------------------------------------------------------
-
-instance Monad (Program extra) where
+instance Monad Program where
   return = Return
   (>>=) = flip Bind
 
+
 ---------------------------------------------------------------------------
--- runP 
+-- Continuation 
 ---------------------------------------------------------------------------
-runP :: Int -> Program extra a -> (a,Int) 
-runP i (Return a) = (a,i)
-runP i (Bind f m) =
-  let (a,i') = runP i m
-  in runP i' (f a) 
-runP i (Sync) = ((),i)
-runP i (ForAll _ _ ) = ((),i)
-runP i (Allocate _ _ _) = ("new" ++ show i,i+1)
-runP i (Assign _ _ _) = ((),i)
-runP i (AtomicOp _ _ _) = (variable ("new"++show i),i+1)
+
+data P a =
+  P {unP :: forall b . (((a -> Program b) -> Program b))}
+
+instance Monad P where
+  return a = P (\k -> k a)
+  P f >>= m = P (\k -> f (\a -> unP (m a) k))
+
+runP p = (unP p) (\_ -> Skip)
+
+forAll :: Word32 -> (Exp Word32 -> P ()) -> P ()
+forAll n body =
+  P $ \k ->
+  do
+    let b = (\i -> unP (body i) (\_ -> Skip)) 
+    ForAll n b >> k () 
+
+
+
+---------------------------------------------------------------------------
+-- runPrg 
+---------------------------------------------------------------------------
+runPrg :: Int -> Program a -> (a,Int) 
+runPrg i (Return a) = (a,i)
+runPrg i (Bind f m) =
+  let (a,i') = runPrg i m
+  in runPrg i' (f a) 
+runPrg i (Sync) = ((),i)
+runPrg i (ForAll _ _ ) = ((),i)
+runPrg i (Allocate _ _) = ("new" ++ show i,i+1)
+runPrg i (Assign _ _ _) = ((),i)
+runPrg i (AtomicOp _ _ _) = (variable ("new"++show i),i+1)
 
 runPAccm :: Monoid b
             => Int
-            -> Program extra a
-            -> (forall a . (Program extra a -> b))
+            -> Program a
+            -> (forall a . (Program a -> b))
             -> b 
             -> (a,b,Int)
 runPAccm i p@(Return a) f b  = (a, f p `mappend` b,i) 
@@ -145,7 +120,7 @@ runPAccm i p@(Bind mf m) f b =
   in  runPAccm i' (mf a) f acc
 runPAccm i p@(Sync) f b = ((),f p `mappend` b, i)
 runPAccm i p@(ForAll _ _) f b = ((),f p `mappend` b, i)
-runPAccm i p@(Allocate _ _ _) f b = ("arr" ++ show i,f p `mappend` b, i)
+runPAccm i p@(Allocate _ _) f b = ("arr" ++ show i,f p `mappend` b, i)
 runPAccm i p@(Assign _ _ _)  f b = ((), f p `mappend` b, i)
 runPAccm i p@(AtomicOp _ _ _) f b =
   (variable ("new" ++ show i),f p `mappend` b,i+1);
@@ -153,23 +128,23 @@ runPAccm i p@(AtomicOp _ _ _) f b =
 -- took same as precedence as for ++ for *>*
 infixr 5 *>* 
 
-(*>*) :: Program extra a 
-         -> Program extra b
-         -> Program extra b   
+(*>*) :: Program a 
+         -> Program b
+         -> Program b   
 (*>*) p1 p2 = p1 >> p2  
     
 
 ---------------------------------------------------------------------------
 -- Required threads (reimplement in runPAccm ?) 
 ---------------------------------------------------------------------------
-programThreads :: Program extra a -> Word32
+programThreads :: Program a -> Word32
 programThreads Skip = 0
 programThreads (Sync) = 0 
 programThreads (Assign _ _ _) = 1
 programThreads (ForAll n f) = n -- inner ForAlls are sequential
-programThreads (Allocate _ _ _) = 0 -- programThreads p
+programThreads (Allocate _ _) = 0 -- programThreads p
 programThreads (Bind f m) =
-  let (a,_) = runP 0 m
+  let (a,_) = runPrg 0 m
   in max (programThreads m) (programThreads (f a)) 
   
 --  max (programThreads p1) (programThreads p2)
@@ -180,7 +155,7 @@ programThreads (AtomicOp _ _ _) = 1
 ---------------------------------------------------------------------------
 -- printProgram 
 ---------------------------------------------------------------------------
-printProgram :: Int -> Program () a -> (String,Int)  
+printProgram :: Int -> Program a -> (String,Int)  
 printProgram i Skip = (";\n", i)
 printProgram i (Assign n ix e) =
   (n ++ "[" ++ show ix ++ "] = " ++ show e ++ ";\n", i) 
@@ -188,7 +163,7 @@ printProgram i (AtomicOp n ix e) =
   let newname = "r" ++ show i
   in (newname ++ " = " ++ printAtomic e ++
       "( " ++ n ++ "[" ++ show ix ++ "])\n",i+1)
-printProgram i (Allocate n t extra) =
+printProgram i (Allocate n t) =
   let newname = "arr" ++ show i
   in (newname ++ " = malloc(" ++ show n ++ ");\n",i+1)
 printProgram i (ForAll n f) =
@@ -198,12 +173,12 @@ printProgram i (ForAll n f) =
 printProgram i (Return a) = ("MonadReturn;\n",i)
 printProgram i (Bind f m) =
   let (str1,i1) = printProgram i m
-      (res,_) = runP 0 m
+      (res,_) = runPrg 0 m
       (str2,i2) = printProgram i1 (f res)
   in (str1 ++ str2, i2)
 printProgram i Sync = ("Sync;\n",i)
 
-printPrg :: Int -> Program () a -> (a,String,Int)  
+printPrg :: Int -> Program a -> (a,String,Int)  
 printPrg i Skip = ((),";\n", i)
 printPrg i (Assign n ix e) =
   ((),n ++ "[" ++ show ix ++ "] = " ++ show e ++ ";\n", i) 
@@ -212,7 +187,7 @@ printPrg i (AtomicOp n ix e) =
   in (variable newname,
       newname ++ " = " ++ printAtomic e ++
       "( " ++ n ++ "[" ++ show ix ++ "])\n",i+1)
-printPrg i (Allocate n t extra) =
+printPrg i (Allocate n t) =
   let newname = "arr" ++ show i
   in (newname,newname ++ " = malloc(" ++ show n ++ ");\n",i+1)
 printPrg i (ForAll n f) =
@@ -224,28 +199,68 @@ printPrg i (ForAll n f) =
 printPrg i (Return a) = (a,"MonadReturn;\n",i)
 printPrg i (Bind f m) =
   let (a1, str1,i1) = printPrg i m
-      -- (res,_) = runP 0 m
       (a2,str2,i2) = printPrg i1 (f a1)
   in (a2,str1 ++ str2, i2)
 printPrg i Sync = ((),"Sync;\n",i)
 
 
 ---------------------------------------------------------------------------
---
+-- Atomic operations 
 ---------------------------------------------------------------------------
-
---targetArray :: Scalar a => Name -> (Exp Word32,Exp a) -> Program () ()
---targetArray n (i,a) = Assign n i a 
-
-
---targetPair :: (Scalar a, Scalar b) => Name -> Name -> (Exp Word32,(Exp a,Exp b)) -> Program () ()
---targetPair n1 n2  (i,(a,b)) = Assign n1 i a *>*
---                              Assign n2 i b
-
--- Atomic operations
-
 data Atomic a where
   AtomicInc :: Atomic (Data Int)
 
 printAtomic AtomicInc = "atomicInc"
 
+
+
+
+
+---------------------------------------------------------------------------
+-- Tests
+---------------------------------------------------------------------------
+
+testPrg1 :: Program () 
+testPrg1 =
+  do
+    somewhere <- Allocate 100 Int 
+    ForAll 100
+           (\i -> Assign somewhere i (variable "hej" :: Exp Int))
+
+testPrg2 :: Program ()
+testPrg2 =
+  do
+    somewhere <- Allocate 175 Int 
+    ForAll 175
+           (\i -> Assign somewhere i (variable "a" :: Exp Int))
+
+testPrg3 :: Program ()
+testPrg3 = testPrg1 >> Sync >> testPrg2 
+
+---------------------------------------------------------------------------
+-- Small push/pull array test
+---------------------------------------------------------------------------
+data PushArray a =
+  PA (((Exp Word32, a) -> Program ()) -> Program ())
+  
+data PullArray a = PU Word32 (Exp Word32 -> a) 
+
+myPullArray :: PullArray (Exp Int) 
+myPullArray = PU 100 (\ix -> (index "input0" ix) )
+
+push1 :: PullArray (Exp Int) -> PushArray (Exp Int)
+push1 (PU n ixf) =
+  PA (\k -> do
+         ForAll n
+           (\i -> k (i,ixf i)))
+
+writeP :: PushArray (Exp Int) -> Program ()
+writeP (PA p) =
+  do
+    name <-  Allocate 100 Int
+    p (target name) 
+
+target :: Name -> (Exp Word32, Exp Int) -> Program ()
+target name (ix,x) = Assign name ix x 
+
+testPrg4 = writeP (push1 myPullArray)
